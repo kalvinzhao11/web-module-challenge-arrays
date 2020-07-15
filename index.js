ab44f9671e78ac1d92143ddea44891e91f392969
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-07-15 12:01:29 -0400

    Added support to add flavors

--- index.js
+++ index.js
@@ -62,12 +62,15 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(flavors, newFlavor){
 
     /*code here*/
+    flavors.unshift(newFlavor)
+    return flavors
 
 }
-
+const coconut = "coconut"
+console.log(addFlavor(originalFlavors, coconut))
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
